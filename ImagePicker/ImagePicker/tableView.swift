ffdcbba79c230cad98bf46cd74819dd59c2da8d0
Ben Johnston <benjohnstonsf@gmail.com>
2015-06-11 19:06:59 -0700

    Set up table

--- ImagePicker/ImagePicker/tableView.swift
+++ ImagePicker/ImagePicker/tableView.swift
@@ -11,6 +11,7 @@ import UIKit
 class tableView: UITableViewController, UITableViewDataSource, UITableViewDelegate {
   
   var memes: [Meme]!
+  var test = ["1", "2", "3", "4", "5"]
   func viewWillAppear() {
     let object = UIApplication.sharedApplication().delegate
     let appDelegate = object as! AppDelegate
@@ -18,5 +19,24 @@ class tableView: UITableViewController, UITableViewDataSource, UITableViewDelega
   }
   
   
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier("memeCell") as! UITableViewCell
+    let num = self.test[indexPath.row]
+    
+    // Set the name and image
+    cell.textLabel?.text = num
+
+    
+    // If the cell has a detail label, we will put the evil scheme in.
+
+    
+    return cell
+  }
 
 }
